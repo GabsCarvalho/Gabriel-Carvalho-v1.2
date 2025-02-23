cc38407bd27c00da3fbc4ca2ea857580f9dc2231
GabsCarvalho <gabrielcarvalho.corp@gmail.com>
2025-02-23 13:11:03 -0400

    Update formulaio.php

--- CODIGOS/PHP/formulaio.php
+++ CODIGOS/PHP/formulaio.php
@@ -1,22 +1,20 @@
 <?php
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $conteudo = $_POST['conteudo'];
-    
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = htmlspecialchars($_POST['email']);
+    $conteudo = htmlspecialchars($_POST['conteudo']);
+
     $para = "gabrielcarvalho.corp@gmail.com";
-    $assunto = "Resposta Portfólio - ".$nome;
+    $assunto = "Resposta Portfólio - " . $nome;
 
-    $corpo = "Mensagem de: ".$email."\n\r\n\r".$conteudo;
+    $corpo = "Mensagem de: $nome <$email>\r\n\r\n$conteudo";
 
-    $cabeca = "From: gabrielcarvalho.corp@gmail.com"."\n\r"."Reply-to".$email."\n\r"."X-Mailer:PHP/".phpversion();
+    $cabeca = "From: gabrielcarvalho.corp@gmail.com\r\n";
+    $cabeca .= "Reply-To: $email\r\n";
+    $cabeca .= "X-Mailer: PHP/" . phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo ("Mensagem enviada com sucesso!");
+    if (mail($para, $assunto, $corpo, $cabeca)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail!";
     }
-    else{
-        echo ("Houve um erro ao enviar o e-mail!");
-    }
-
-
-
 ?>
